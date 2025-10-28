cf0f079e93ef00dc7444176819f41455ff43cdf4
r1ckhdk <rick@riseup.net>
2025-10-28 17:22:00 -0300

    Remove conceal from markdown files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,3 +7,11 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- get markdown to always show source code on all lines
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown" },
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
+})

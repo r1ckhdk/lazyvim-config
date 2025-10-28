4b592c7f12791d6bdc37622bb04adebd32dea0b6
r1ckhdk <rick@riseup.net>
2025-10-28 12:39:37 -0300

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.spell = false
+
+-- Ativa wrap visual
+vim.wo.wrap = true
+
+-- Quebra linhas nos espaços (não no meio de uma palavra)
+vim.wo.linebreak = true
+
+-- Faz com que a quebra de linha se alinhe com a indentação
+vim.wo.breakindent = true

551bf97da11a7660cdccf8fdab54eb1fd8af2564
r1ckhdk <rick@riseup.net>
2026-02-11 19:00:51 -0300

    add zenmode plugin

---
+++ lua/plugins/zenmode.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/zen-mode.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    window = {
+      backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+      width = 90, -- width of the Zen window
+      height = 1, -- height of the Zen window
+    },
+  }
+}
